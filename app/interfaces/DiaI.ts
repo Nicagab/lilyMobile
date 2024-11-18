442b721ff716a429d0e98a2ca082405b4a4a80b9
Gabriela Garcia <114267576+Nicagab@users.noreply.github.com>
2024-11-18 10:33:26 -0300

    Add files via upload

---
+++ app/interfaces/DiaI.ts
@@ -0,0 +1,9 @@
+interface Dia {
+    numero?: number;
+    dataZ?: string;
+    classe?: string; 
+    emocional?: string;
+    anotacao?: string;
+}
+
+export default Dia
---
+++ app/interfaces/UsuarioSimplesI.ts
@@ -0,0 +1,11 @@
+interface UsuarioSimples {
+    idUsuario?: number,
+    username?: string,
+    email?: string,
+    senha?: string,
+    nome?: string,
+    dataNasc?: string,
+    tipo?: string,
+}
+
+export default UsuarioSimples
